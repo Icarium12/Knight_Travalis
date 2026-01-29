60a0d4df981cbf8fd15f549775cb10c08b8c9548
Praise Mogaji <praisemogaji@gmail.com>
2026-01-29 06:37:21 +0100

    Created moves for the game board

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ for (let i = 0; i < 8; i++) {
     board[i] = [];
     for (let j = 0; j < 8; j++) {
         board[i][j] = new Node;
+        board[i][j].value = [i, j];
     }
 }
-
 
 
 
@@ -26,7 +26,7 @@ for (let i = 0; i < board.length; i++) {
             board[i][j].left === null
         }
         else {
-            board[i][j].left === board[i][j-1];
+            board[i][j].left = board[i][j-1];
         }
 
         if (j + 1 > 7) {
@@ -52,4 +52,161 @@ for (let i = 0; i < board.length; i++) {
     }
 }
 
-console.log(board[0][0]);
+
+function knightMoves(start, end, queue = [], visited = []) {
+
+}
+
+const queue = [];
+const visited = [];
+
+queue.push(board[3][3]);
+
+console.log(downRight(queue, visited));
+
+function sideLeftUp(queue, visited, count = 0) {
+    if (count === 2) {
+        const node = queue.shift();
+        // visited.push(node);
+        visited.push(node.up);
+        console.log(visited.length);
+        visited.forEach(node => {
+            console.log(node.value);
+        });
+        return visited;
+    }
+    const node = queue.shift();
+    visited.push(node.left);
+    queue.push(node.left);
+    count++;
+    return sideLeftUp(queue, visited, count);
+
+}
+
+function sideRightUp(queue, visited, count = 0) {
+    if (count === 2) {
+        const node = queue.shift();
+        // visited.push(node);
+        visited.push(node.up);
+        console.log(visited.length);
+        visited.forEach(node => {
+            console.log(node.value);
+        });
+        return visited;
+    }
+    const node = queue.shift();
+    visited.push(node.right);
+    queue.push(node.right);
+    count++;
+    return sideRightUp(queue, visited, count);
+
+}
+
+function sideLeftDown(queue, visited, count = 0) {
+    if (count === 2) {
+        const node = queue.shift();
+        visited.push(node.down);
+        console.log(visited.length);
+        visited.forEach(node => {
+            console.log(node.value);
+        });
+        return visited;
+    }
+    const node = queue.shift();
+    visited.push(node.left);
+    queue.push(node.left);
+    count++;
+    return sideLeftDown(queue, visited, count);
+
+}
+
+
+function sideRightDown(queue, visited, count = 0) {
+    if (count === 2) {
+        const node = queue.shift();
+        visited.push(node.down);
+        console.log(visited.length);
+        visited.forEach(node => {
+            console.log(node.value);
+        });
+        return visited;
+    }
+    const node = queue.shift();
+    visited.push(node.right);
+    queue.push(node.right);
+    count++;
+    return sideRightDown(queue, visited, count);
+}
+
+
+function upLeft(queue, visited, count = 0) {
+    if (count === 2) {
+        const node = queue.shift();
+        visited.push(node.left);
+        console.log(visited.length);
+        visited.forEach(node => {
+            console.log(node.value);
+        });
+        return visited;
+    }
+    const node = queue.shift();
+    visited.push(node.up);
+    queue.push(node.up);
+    count++;
+    return upLeft(queue, visited, count);
+
+}
+
+function upRight(queue, visited, count = 0) {
+    if (count === 2) {
+        const node = queue.shift();
+        visited.push(node.right);
+        console.log(visited.length);
+        visited.forEach(node => {
+            console.log(node.value);
+        });
+        return visited;
+    }
+    const node = queue.shift();
+    visited.push(node.up);
+    queue.push(node.up);
+    count++;
+    return upRight(queue, visited, count);
+
+}
+
+function downLeft(queue, visited, count = 0) {
+    if (count === 2) {
+        const node = queue.shift();
+        visited.push(node.left);
+        console.log(visited.length);
+        visited.forEach(node => {
+            console.log(node.value);
+        });
+        return visited;
+    }
+    const node = queue.shift();
+    visited.push(node.down);
+    queue.push(node.down);
+    count++;
+    return downLeft(queue, visited, count);
+
+}
+
+function downRight(queue, visited, count = 0) {
+    if (count === 2) {
+        const node = queue.shift();
+        visited.push(node.right);
+        console.log(visited.length);
+        visited.forEach(node => {
+            console.log(node.value);
+        });
+        return visited;
+    }
+    const node = queue.shift();
+    visited.push(node.down);
+    queue.push(node.down);
+    count++;
+    return downRight(queue, visited, count);
+
+}
